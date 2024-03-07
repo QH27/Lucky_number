c050c2e4545d857a2dd6af33555c4012a88e2ffb
QH27 <2151060231@e.tlu.edu.vn>
2024-03-07 22:53:37 +0700

    add menu funtion

--- app/src/main/java/com/example/lucky_number_app/MainActivity.java
+++ app/src/main/java/com/example/lucky_number_app/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.lucky_number_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText editText;
@@ -31,5 +35,28 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_underscore_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemID = item.getItemId();
+        if(itemID == R.id.action_home){
+            Toast.makeText(
+                    this,
+                    "You selected Home",
+                    Toast.LENGTH_SHORT).show();
+        } else if (itemID == R.id.action_search) {
+            Toast.makeText(
+                    this,
+                    "You selected Search",
+                    Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
